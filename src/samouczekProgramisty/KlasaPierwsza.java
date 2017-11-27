64ec731e17e33fb4236c15e17fa15da6666f3a54
Anna Zaremba <aniazarem@gmail.com>
2017-11-27 21:30:41 +0100

    samouczekProgramisty

---
+++ src/samouczekProgramisty/KlasaPierwsza.java
@@ -0,0 +1,10 @@
+package samouczekProgramisty;
+
+public class KlasaPierwsza {
+
+    public static void main(String[] args) {
+        String message = "To jest mój pierwszy łańcuch znaków! Potocznie \"string\".";
+        System.out.println(message);
+    }
+
+}
