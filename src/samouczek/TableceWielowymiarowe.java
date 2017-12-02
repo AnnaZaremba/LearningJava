78c034a817f503d0a44b8881813c5e26711886d0
Anna Zaremba <aniazarem@gmail.com>
2017-12-02 19:24:46 +0100

    tabliece wielowymiarowe

--- src/samouczek/TableceWielowymiarowe.java
+++ src/samouczek/TableceWielowymiarowe.java
@@ -22,6 +22,17 @@ public class TableceWielowymiarowe {
         System.out.println("a   " + kolkoKrzyzyk[0][0] + "|" + kolkoKrzyzyk[0][1] + "|" + kolkoKrzyzyk[0][2]);
         System.out.println("b   " + kolkoKrzyzyk[1][0] + "|" + kolkoKrzyzyk[1][1] + "|" + kolkoKrzyzyk[1][2]);
         System.out.println("c   " + kolkoKrzyzyk[2][0] + "|" + kolkoKrzyzyk[2][1] + "|" + kolkoKrzyzyk[2][2]);
+
+//        Napisz metodę pobierającą dwuelementową tablicę liczb i zwracający ich sumę
+
+        int[] sumaDwoch = new int[2];
+
+        sumaDwoch[0] = 23;
+        sumaDwoch[1] = 7;
+
+        System.out.println("Suma dwóch liczb jest równa: " + (sumaDwoch[0] + sumaDwoch[1]));
+
+
     }
 
 }
