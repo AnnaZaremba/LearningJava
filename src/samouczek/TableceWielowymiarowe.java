6d422776223e7890b2e3d63a4bd9183b7e267996
Anna Zaremba <aniazarem@gmail.com>
2017-12-02 20:10:46 +0100

    tabliece sortowanie

--- src/samouczek/TableceWielowymiarowe.java
+++ src/samouczek/TableceWielowymiarowe.java
@@ -1,8 +1,25 @@
 package samouczek;
 
+import java.util.Arrays;
+
 public class TableceWielowymiarowe {
 
     public static void main(String[] args) {
+
+        /*
+            ------------------------------------
+               [0][0]   |  [0][1] |   [0][2]   |
+            ------------------------------------
+            ------------------------------------
+               [1][0]   |  [1][1] |   [1][2]   |
+            ------------------------------------
+            ------------------------------------
+               [2][0]   |  [2][1] |   [2][2]   |
+            ------------------------------------
+            ------------------------------------
+               [3][0]   |  [3][1] |   [3][2]   |
+            ------------------------------------
+         */
 
         int[][] kolkoKrzyzyk = new int[3][3];
 
@@ -23,7 +40,7 @@ public class TableceWielowymiarowe {
         System.out.println("b   " + kolkoKrzyzyk[1][0] + "|" + kolkoKrzyzyk[1][1] + "|" + kolkoKrzyzyk[1][2]);
         System.out.println("c   " + kolkoKrzyzyk[2][0] + "|" + kolkoKrzyzyk[2][1] + "|" + kolkoKrzyzyk[2][2]);
 
-//        Napisz metodę pobierającą dwuelementową tablicę liczb i zwracający ich sumę
+        // Napisz metodę pobierającą dwuelementową tablicę liczb i zwracający ich sumę
 
         int[] sumaDwoch = new int[2];
 
@@ -32,6 +49,33 @@ public class TableceWielowymiarowe {
 
         System.out.println("Suma dwóch liczb jest równa: " + (sumaDwoch[0] + sumaDwoch[1]));
 
+        // Napisz program pobierający trójelementową tablicę liczb i zwracający największą liczbę.
+
+        int[] trzyLiczby = new int[3];
+
+        trzyLiczby[0] = 4;
+        trzyLiczby[1] = 34;
+        trzyLiczby[2] = 14;
+
+        Arrays.sort(trzyLiczby);
+
+        System.out.println("Sortowanie od najwiekszej do najmniejszej: " + Arrays.toString(trzyLiczby));
+        System.out.println("Największa liczba z tablicy to: " + trzyLiczby[2]);
+
+        int[] array = new int[3];
+
+        // Liczby losowe w tablicy
+
+        System.out.println(Math.random());
+        int max = 50;
+        array[0] = (int) (max * Math.random());
+        array[1] = (int) (max * Math.random());
+        array[2] = (int) (max * Math.random());
+
+        int result = Arrays.stream(array).max().getAsInt();
+
+        System.out.println("Sortowanie od najwiekszej do najmniejszej: " + Arrays.toString(array));
+        System.out.println("Największa liczba z tablicy to: " + result);
 
     }
 
